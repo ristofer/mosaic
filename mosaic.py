6bb9bde062c7d8a027e90dae27be4535dc821c6e
Cristopher <cristopher.gomez@ug.uchile.cl>
2020-06-25 16:47:20 -0400

    sadf

--- mosaic.py
+++ mosaic.py
@@ -3,11 +3,12 @@ import os
 from PIL import Image
 from multiprocessing import Process, Queue, cpu_count
 import copy
+import json
 Image.MAX_IMAGE_PIXELS = None
 # Change these 3 config parameters to suit your needs...
-TILE_SIZE      = 100		# height/width of mosaic tiles in pixels
+TILE_SIZE      = 20		# height/width of mosaic tiles in pixels
 TILE_MATCH_RES = 1		# tile matching resolution (higher values give better fit but require more processing)
-ENLARGEMENT    = 24		# the mosaic image will be this many times wider and taller than the original
+ENLARGEMENT    = 4		# the mosaic image will be this many times wider and taller than the original
 
 TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
 WORKER_COUNT = max(cpu_count() - 1, 1)
@@ -18,7 +19,7 @@ COUNTER = 0
 class TileProcessor:
 	def __init__(self, tiles_directory):
 		self.tiles_directory = tiles_directory
-
+		self.tiles_paths = []
 	def __process_tile(self, tile_path):
 		try:
 			img = Image.open(tile_path)
@@ -52,6 +53,7 @@ class TileProcessor:
 				if large_tile:
 					large_tiles.append(large_tile)
 					small_tiles.append(small_tile)
+					self.tiles_paths.append(tile_path)
 
 
 		print('Processed {} tiles.'.format(len(large_tiles)))
@@ -170,6 +172,7 @@ class MosaicImage:
 
 def build_mosaic(result_queue, all_tile_data_large, original_img_large,outpath):
 	mosaic = MosaicImage(original_img_large,outpath)
+	tablita = dict()
 
 	active_workers = WORKER_COUNT
 	while True:
@@ -183,11 +186,14 @@ def build_mosaic(result_queue, all_tile_data_large, original_img_large,outpath):
 			else:
 				tile_data = all_tile_data_large[best_fit_tile_index]
 				mosaic.add_tile(tile_data, img_coords)
+				tablita[str(img_coords)] = best_fit_tile_index
 
 		except KeyboardInterrupt:
 			pass
 
 	mosaic.save(mosaic.pathito)
+	with open(mosaic.pathito+".json", "w") as write_file:
+		json.dump(tablita, write_file)
 	print('\nFinished, output is in', OUT_FILE)
 
 def compose(original_img, tiles,outpath):
@@ -228,10 +234,17 @@ def compose(original_img, tiles,outpath):
 		for n in range(WORKER_COUNT):
 			work_queue.put((EOQ_VALUE, EOQ_VALUE))
 
-def mosaic(img_path, tiles_path,outpath):
-	image_data = TargetImage(img_path).get_data()
-	tiles_data = TileProcessor(tiles_path).get_tiles()
-	compose(image_data, tiles_data,outpath)
+def mosaic(img_path, tiles_path):
+	tp = TileProcessor(tiles_path)
+	tiles_data = tp.get_tiles()
+	tiles_pathsitos = tp.tiles_paths
+	with open("paths.json", "w") as write_file:
+		json.dump(tiles_pathsitos, write_file)
+	for wea in tiles_pathsitos:
+		image_data = TargetImage(wea).get_data()
+		flp = wea.split("/")[-1]
+		compose(image_data, tiles_data,flp)
+
 #import os, sys
 
 #path = "/home/tof/hash/fundacionmustakis/"
@@ -251,5 +264,5 @@ if __name__ == '__main__':
 	if len(sys.argv) < 4:
 		print('Usage: {} <image> <tiles directory>\r'.format(sys.argv[0]))
 	else:
-		mosaic(sys.argv[1], sys.argv[2],"mosaic{}.jpeg".format(sys.argv[3]))
+		mosaic(sys.argv[1], sys.argv[2])
 
